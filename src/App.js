4629ecc9b2195be7dd109561ef74494c09e152be
kshitiz bakshi <bakshikshitiz@gmail.com>
2021-12-23 17:18:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/navbar";
+import "./App.css";
+import Header from "./components/header";
+import Movies from "./components/movies";
+import Result from "./components/result";
+import { useState } from "react";
+import { useEffect } from "react";
+import { BrowserRouter as Router, Route ,Switch} from 'react-router-dom'
 
 function App() {
+  const key = "e1e16497";
+  const apiurl = "https://www.omdbapi.com/?apikey=" + key;
+
+  const [movieTitle, setmovieTitle] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    fetch(apiurl + "&s=" + title)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.Search) {
+          setMovies(data.Search);
+        } else {
+          setMovies([]);
+        }
+      });
+  }, [title]);
+
+  const getTitle=(movieT)=>{
+      setmovieTitle(movieT);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+       <Navbar />
+      <Switch>
+          <Route exact path="/" render={()=>{
+            return(
+            <>
+            <Header setTitle={setTitle} title={title} />
+            <Movies movies={movies} getTitle={getTitle} />
+            </>)
+          }}> 
+          </Route>
+          <Route exact path="/result">
+            <Result movieTitle={movieTitle}/>
+          </Route> 
+        </Switch> 
+    </Router>
+    </>
   );
 }
 
